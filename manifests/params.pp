6508a65a0ab757e8587f2dd9d53540dc55e9537f
MCarmen Correa <mcorrea@tedial.com>
2019-04-09 12:52:19 +0200

    Error: Cannot retrieve repository metadata (repomd.xml) for repository: epel.

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,15 +21,15 @@ class epel::params {
       $url_path = 'mirrorlist'
   }
 
-  $epel_mirrorlist                        = "https://mirrors.fedoraproject.org/${url_path}?repo=epel-${os_maj_release}&arch=\$basearch"
-  $epel_baseurl                           = 'absent'
+  $epel_mirrorlist                        = 'absent'
+  $epel_baseurl                           = "http://download.fedoraproject.org/pub/epel/6/\$basearch"
   $epel_failovermethod                    = 'priority'
   $epel_proxy                             = $proxy
   $epel_enabled                           = '1'
   $epel_gpgcheck                          = '1'
   $epel_exclude                           = 'ast'
-  $epel_testing_mirrorlist                = "https://mirrors.fedoraproject.org/${url_path}?repo=testing-epel${os_maj_release}&arch=\$basearch"
-  $epel_testing_baseurl                   = 'absent'
+  $epel_testing_mirrorlist                = 'absent' 
+  $epel_testing_baseurl                   = "http://download.fedoraproject.org/pub/epel/testing/6/$basearch"
   $epel_testing_failovermethod            = 'priority'
   $epel_testing_proxy                     = $proxy
   $epel_testing_enabled                   = '0'
@@ -42,15 +42,15 @@ class epel::params {
   $epel_source_enabled                    = '0'
   $epel_source_gpgcheck                   = '1'
   $epel_source_exclude                    = 'ast'
-  $epel_debuginfo_mirrorlist              = "https://mirrors.fedoraproject.org/${url_path}?repo=epel-debug-${os_maj_release}&arch=\$basearch"
-  $epel_debuginfo_baseurl                 = 'absent'
+  $epel_debuginfo_mirrorlist              = 'absent'
+  $epel_debuginfo_baseurl                 = "http://download.fedoraproject.org/pub/epel/6/\$basearch/debug"
   $epel_debuginfo_failovermethod          = 'priority'
   $epel_debuginfo_proxy                   = $proxy
   $epel_debuginfo_enabled                 = '0'
   $epel_debuginfo_gpgcheck                = '1'
   $epel_debuginfo_exclude                 = 'ast'
-  $epel_testing_source_mirrorlist         = "https://mirrors.fedoraproject.org/${url_path}?repo=testing-source-epel${os_maj_release}&arch=\$basearch"
-  $epel_testing_source_baseurl            = 'absent'
+  $epel_testing_source_mirrorlist         = 'absent'
+  $epel_testing_source_baseurl            = "http://download.fedoraproject.org/pub/epel/6/SRPMS"
   $epel_testing_source_failovermethod     = 'priority'
   $epel_testing_source_proxy              = $proxy
   $epel_testing_source_enabled            = '0'
